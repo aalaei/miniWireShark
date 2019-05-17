3f14494cd0e946f6afd21c06100be7f311beeb67
ali <a@a.a>
2019-05-17 09:17:04 +0430

    arp added

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import socket
 from struct import unpack, pack
 import textwrap
 import platform
-
-TAB = '\t - '
+import binascii
 
 
 class Pcap:
@@ -101,6 +100,14 @@ class UDP:
     def __init__(self, raw_data):
         self.src_port, self.dest_port, self.size, self.checkSum = unpack('! H H H H', raw_data[:8])
         self.data = raw_data[8:]
+
+
+class ARP:
+
+    def __init__(self, raw_data):
+        self.Hardware_Type, self.ProtoclType, self.HardwareAdressSize, self.ProtocolAddressSize, self.Operation, self.Sender_MAC, self.Sender_IP, self.dst_mac, self.dst_IP = unpack(
+            '2s 2s 1s 1s 2s 6s 4s 6s 4s', raw_data[:30])
+        self.data = raw_data[28:]
 
 
 class DnsQuery:
@@ -278,196 +285,210 @@ def make_connection():
     return conn
 
 
-
-
 def main():
-    pcap = Pcap('capture.pcap')
+    pcap = Pcap('captured.pcap')
 
     conn = make_connection()
-    # try:
-    while True:
+    try:
+        while True:
 
-        raw_data, addr = conn.recvfrom(65535)
-        pcap.write(raw_data)
+            raw_data, addr = conn.recvfrom(65535)
+            pcap.write(raw_data)
 
-        # ethernet
-        eth = Ethernet(raw_data)
+            # ethernet
+            eth = Ethernet(raw_data)
+            print('\nEthernet Frame:')
+            print('\t - ' + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac, eth.proto))
 
-        print('\nEthernet Frame:')
-        print(TAB + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac, eth.proto))
+            # arp
+            # if eth.proto == '\x08\x06':
+            if eth.proto == 1544 or eth.proto == '\x08\x06':
+                arp = ARP(eth.data)
+                print("ARP_HEADER")
+                print("Hardware type:   ", binascii.hexlify(arp.Hardware_Type))
+                print("Protocol type:   ", binascii.hexlify(arp.ProtoclType))
+                print("Hardware size:   ", binascii.hexlify(arp.HardwareAdressSize))
+                print("Protocol size:   ", binascii.hexlify(arp.ProtoclType))
+                print("Opcode:          ", binascii.hexlify(arp.Operation))
+                print("Source MAC:      ", binascii.hexlify(arp.Sender_MAC))
+                print("Source IP:      ", binascii.hexlify(arp.Sender_IP))
+                print("Dest MAC:      ", binascii.hexlify(arp.dst_mac))
+                print("Dest IP:      ", binascii.hexlify(arp.dst_IP))
 
-        # IP
-        if eth.proto == 8:
-            ipv4 = IPv4(eth.data)
-            print(TAB + 'IPv4 Packet:')
-            print("\t" + TAB + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length,
-                                                                                 ipv4.ttl))
-            print("\t" + TAB + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
+                continue
 
-            # ICMP Protocol
-            if ipv4.proto == 1:
-                icmp = ICMP(ipv4.data)
-                print(TAB + 'ICMP Packet:')
-                print("\t" + TAB + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type, icmp.code, icmp.checksum))
-                if icmp.type == 0:
-                    print("Echo Reply")
-                elif icmp.type == 3:
-                    print("Destination Unreachable")
-                    if icmp.code == 0:
-                        print("Net Unreachable")
-                    elif icmp.code == 1:
-                        print("HOST Unreachable")
-                    elif icmp.code == 2:
-                        print("Protocol Unreachable")
-                    elif icmp.code == 3:
-                        print("Port Unreachable")
-                    elif icmp.code == 4:
-                        print(" Cannot Fragment")
-                    else:
-                        print("Source Route Failed")
+            # IP
+            if eth.proto == 8:
+                ipv4 = IPv4(eth.data)
+                print('\t - ' + 'IPv4 Packet:')
+                print("\t" + '\t - ' + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length,
+                                                                                     ipv4.ttl))
+                print("\t" + '\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
 
-                elif icmp.type == 4:
-                    print("Source Quench")
-                elif icmp.type == 5:
-                    print("Redirect")
-                    if icmp.code == 0:
-                        print("Redirect datagrams for the network")
-                    elif icmp.code == 1:
-                        print("Redirect datagrams for the host")
-                    elif icmp.code == 2:
-                        print("Redirect datagrams for the Type of Service and the network")
-                    elif icmp.code == 3:
-                        print("Redirect datagrams for the Type of Service and the host")
+                # ICMP Protocol
+                if ipv4.proto == 1:
+                    icmp = ICMP(ipv4.data)
+                    print('\t - ' + 'ICMP Packet:')
+                    print("\t" + '\t - ' + 'Type: {}, Code: {}, Checksum: {},'.format(icmp.type, icmp.code, icmp.checksum))
+                    if icmp.type == 0:
+                        print("Echo Reply")
+                    elif icmp.type == 3:
+                        print("Destination Unreachable")
+                        if icmp.code == 0:
+                            print("Net Unreachable")
+                        elif icmp.code == 1:
+                            print("HOST Unreachable")
+                        elif icmp.code == 2:
+                            print("Protocol Unreachable")
+                        elif icmp.code == 3:
+                            print("Port Unreachable")
+                        elif icmp.code == 4:
+                            print(" Cannot Fragment")
+                        else:
+                            print("Source Route Failed")
 
-                elif icmp.type == 8:
-                    print("Echo Request")
-                elif icmp.type == 11:
-                    print("Time Exceeded ")
-                elif icmp.type == 12:
-                    print("Parameter Problem")
-                elif icmp.type == 13:
-                    print("Timestamp request")
-                elif icmp.type == 14:
-                    print("Timestamp reply ")
-                elif icmp.type == 15:
-                    print("Information Request")
+                    elif icmp.type == 4:
+                        print("Source Quench")
+                    elif icmp.type == 5:
+                        print("Redirect")
+                        if icmp.code == 0:
+                            print("Redirect datagrams for the network")
+                        elif icmp.code == 1:
+                            print("Redirect datagrams for the host")
+                        elif icmp.code == 2:
+                            print("Redirect datagrams for the Type of Service and the network")
+                        elif icmp.code == 3:
+                            print("Redirect datagrams for the Type of Service and the host")
 
-                elif icmp.type == 16:
-                    print("Information Reply")
+                    elif icmp.type == 8:
+                        print("Echo Request")
+                    elif icmp.type == 11:
+                        print("Time Exceeded ")
+                    elif icmp.type == 12:
+                        print("Parameter Problem")
+                    elif icmp.type == 13:
+                        print("Timestamp request")
+                    elif icmp.type == 14:
+                        print("Timestamp reply ")
+                    elif icmp.type == 15:
+                        print("Information Request")
 
-                elif icmp.type == 17:
-                    print("Address mask request ")
+                    elif icmp.type == 16:
+                        print("Information Reply")
 
-                elif icmp.type == 18:
-                    print("Address mask response")
+                    elif icmp.type == 17:
+                        print("Address mask request ")
 
-                print("\t" + TAB + 'ICMP Data:')
-                print(format_multi_line("\t\t\t   ", icmp.data))
+                    elif icmp.type == 18:
+                        print("Address mask response")
 
-            # TCP Protocol
-            elif ipv4.proto == 6:
-                tcp = TCP(ipv4.data)
-                print(TAB + 'TCP Segment:')
-                print("\t" + TAB + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
-                print("\t" + TAB + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence, tcp.acknowledgment))
-                print("\t" + TAB + 'Flags:')
-                print("\t\t" + TAB + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack, tcp.flag_psh))
-                print("\t\t" + TAB + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn, tcp.flag_fin))
+                    print("\t" + '\t - ' + 'ICMP Data:')
+                    print(format_multi_line("\t\t\t   ", icmp.data))
 
-                if len(tcp.data) > 0:
+                # TCP Protocol
+                elif ipv4.proto == 6:
+                    tcp = TCP(ipv4.data)
+                    print('\t - ' + 'TCP Segment:')
+                    print("\t" + '\t - ' + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port, tcp.dest_port))
+                    print("\t" + '\t - ' + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence, tcp.acknowledgment))
+                    print("\t" + '\t - ' + 'Flags:')
+                    print("\t\t" + '\t - ' + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack, tcp.flag_psh))
+                    print("\t\t" + '\t - ' + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn, tcp.flag_fin))
 
-                    # HTTP
-                    if tcp.src_port == 80 or tcp.dest_port == 80:
-                        print("\t" + TAB + 'HTTP Data:')
+                    if len(tcp.data) > 0:
+
+                        # HTTP
+                        if tcp.src_port == 80 or tcp.dest_port == 80:
+                            print("\t" + '\t - ' + 'HTTP Data:')
+                            try:
+                                http = HTTP(tcp.data)
+                                http_info = str(http.data).split('\n')
+                                for line in http_info:
+                                    print("\t\t\t   " + str(line))
+                            except:
+                                print(format_multi_line("\t\t\t   ", tcp.data))
+                        elif tcp.src_port == 27 or tcp.src_port == 28 or tcp.dest_port == 27 or tcp.dest_port == 28:
+
+                            print("\t" + '\t - ' + 'FTP Data:')
                         try:
-                            http = HTTP(tcp.data)
-                            http_info = str(http.data).split('\n')
-                            for line in http_info:
+                            ftp = FTP(tcp.data)
+                            ftp_info = str(ftp.data).split('\n')
+                            for line in ftp_info:
                                 print("\t\t\t   " + str(line))
                         except:
                             print(format_multi_line("\t\t\t   ", tcp.data))
-                    elif tcp.src_port == 27 or tcp.src_port == 28 or tcp.dest_port == 27 or tcp.dest_port == 28:
-
-                        print("\t" + TAB + 'FTP Data:')
-                    try:
-                        ftp = FTP(tcp.data)
-                        ftp_info = str(ftp.data).split('\n')
-                        for line in ftp_info:
-                            print("\t\t\t   " + str(line))
-                    except:
-                        print(format_multi_line("\t\t\t   ", tcp.data))
-                    else:
-                        print("\t" + TAB + 'TCP Data:')
-
-                        print(format_multi_line("\t\t\t   ", tcp.data))
-
-
-            # UDP Protocol
-            elif ipv4.proto == 17:
-                udp = UDP(ipv4.data)
-                print(TAB + 'UDP Segment:')
-                print("\t" + TAB + 'Source Port: {}, Destination Port: {}, Length: {}'.format(udp.src_port,
-                                                                                              udp.dest_port,
-                                                                                              udp.size))
-                # DNS Protocol
-                if udp.src_port == 53 or udp.dest_port == 53:
-                    print("DNS Data: ")
-                    try:
-                        dns = DNS(udp.data)
-                        print("Identification: {} ,Control: {}".format(dns.indentification, dns.control))
-                        if dns.QR:
-                            print("Response")
                         else:
-                            print("Query")
-                        print("num of Questions:{}".format(dns.Question_count))
+                            print("\t" + '\t - ' + 'TCP Data:')
 
-                        print("num of Answers:{}".format(dns.Answer_count))
-                        print("num of Authorities:{}".format(dns.totalAuthority_RR))
-                        print("num of Additional RRs :{}".format(dns.TotalAditional_RR))
-                        print("Questions: ")
-                        print("------------------------")
-                        qs = dns.questions
-                        for q in qs:
-                            print(q.Query_Name)
+                            print(format_multi_line("\t\t\t   ", tcp.data))
 
-                        # print(dns.questions)
-                        print("Answers:")
-                        print("------------------------")
-                        qs = dns.answersRRs
-                        for q in qs:
-                            print(q.Name)
 
-                        # print(dns.answersRRs)
-                        print("Authorities:")
-                        print("------------------------")
-                        qs = dns.authorityRRs
-                        for q in qs:
-                            print(q.Name)
-                        # print(dns.authorityRRs)
-                        print("Additional")
-                        qs = dns.aditionalRRs
-                        for q in qs:
-                            print(q.Name)
-                        # print(dns.aditionalRRs)
-                        print("___________________________________")
-                    except:
-                        print(format_multi_line("\t\t\t   ", udp.data))
+                # UDP Protocol
+                elif ipv4.proto == 17:
+                    udp = UDP(ipv4.data)
+                    print('\t - ' + 'UDP Segment:')
+                    print("\t" + '\t - ' + 'Source Port: {}, Destination Port: {}, Length: {}'.format(udp.src_port,
+                                                                                                  udp.dest_port,
+                                                                                                  udp.size))
+                    # DNS Protocol
+                    if udp.src_port == 53 or udp.dest_port == 53:
+                        print("DNS Data: ")
+                        try:
+                            dns = DNS(udp.data)
+                            print("Identification: {} ,Control: {}".format(dns.indentification, dns.control))
+                            if dns.QR:
+                                print("Response")
+                            else:
+                                print("Query")
+                            print("num of Questions:{}".format(dns.Question_count))
+
+                            print("num of Answers:{}".format(dns.Answer_count))
+                            print("num of Authorities:{}".format(dns.totalAuthority_RR))
+                            print("num of Additional RRs :{}".format(dns.TotalAditional_RR))
+                            print("Questions: ")
+                            print("------------------------")
+                            qs = dns.questions
+                            for q in qs:
+                                print(q.Query_Name)
+
+                            # print(dns.questions)
+                            print("Answers:")
+                            print("------------------------")
+                            qs = dns.answersRRs
+                            for q in qs:
+                                print(q.Name)
+
+                            # print(dns.answersRRs)
+                            print("Authorities:")
+                            print("------------------------")
+                            qs = dns.authorityRRs
+                            for q in qs:
+                                print(q.Name)
+                            # print(dns.authorityRRs)
+                            print("Additional")
+                            qs = dns.aditionalRRs
+                            for q in qs:
+                                print(q.Name)
+                            # print(dns.aditionalRRs)
+                            print("___________________________________")
+                        except:
+                            print(format_multi_line("\t\t\t   ", udp.data))
+                    else:
+                        print("UDP data:")
+                        print(udp.data)
+
+                # Other IPv4
                 else:
-                    print("UDP data:")
-                    print(udp.data)
+                    print('\t - ' + 'Unknown IPv4 Data:')
+                    print(format_multi_line("\t\t   ", ipv4.data))
 
-            # Other IPv4
             else:
-                print(TAB + 'Unknown IPv4 Data:')
-                print(format_multi_line("\t\t   ", ipv4.data))
-
-        else:
-            print('Ethernet Data:')
-            print(format_multi_line("\t   ", eth.data))
-    #    except:
-    #       pass
-    #  finally:
-    pcap.close()
+                print('Ethernet Data:')
+                print(format_multi_line("\t   ", eth.data))
+    except:
+        pass
+    finally:
+        pcap.close()
 
 
 main()
